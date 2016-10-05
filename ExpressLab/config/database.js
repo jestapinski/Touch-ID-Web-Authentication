e86da688898141f29a0de680caf23aef6ff4db33
Jordan Stapinski <jstapins@andrew.cmu.edu>
2016-10-05 09:43:21 -0400

    Added login demo using mongoDB

---
+++ ExpressLab/config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    'url' : 'mongodb://jestapinski:15421touchid@ds049496.mlab.com:49496/touchidlogin' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TouchIDAuthentication
 TouchId Authentication project for CMU 15-421
+
+10/05/16 - Added Non-Socket Login Page Demo using MongoDB
