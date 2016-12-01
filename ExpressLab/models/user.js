51f35c958918b27912bfa1d2c6e4b6197e8a7a0d
Akash Kejriwal <akash@kejriwal.com>
2016-12-01 14:26:32 -0500

    removed apple mac from project

--- ExpressLab/models/user.js
+++ ExpressLab/models/user.js
@@ -8,8 +8,7 @@ var userSchema = mongoose.Schema({
 
     local            : {
         email        : String,
-        password     : String,
-        apple_mac    : String, 
+        password     : String
     }
 });
 
@@ -19,19 +18,10 @@ userSchema.methods.generateHash = function(password) {
     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
-userSchema.methods.generateHashForAppleMac = function(apple_mac) {
-    return bcrypt.hashSync(apple_mac, bcrypt.genSaltSync(8), null);
-};
-
 // checking if password is valid
 userSchema.methods.validPassword = function(password) {
     return bcrypt.compareSync(password, this.local.password);
 };
-
-userSchema.methods.validPasswordForAppleMac = function(apple_mac) {
-    return bcrypt.compareSync(apple_mac, this.local.apple_mac);
-};
-
 
 // create the model for users and expose it to our app
 module.exports = mongoose.model('User', userSchema);
